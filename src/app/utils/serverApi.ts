e2dd0edadd02cd5507f3035fee85974d505e963a
김민규 <tjfvndfud@gmail.com>
2024-10-02 18:47:47 +0900

    add: getItems 추가

--- src/app/utils/serverApi.ts
+++ src/app/utils/serverApi.ts
@@ -1,5 +1,6 @@
 import { RIOT_BASE_URL } from "../api/apiKey"
 import { ChampionInfo } from "../types/Champion";
+import { ItemType } from "../types/Item";
 
 // 최신 버전
 export const getLatestVersionUrl = async () => {
@@ -28,3 +29,16 @@ export const getChampions = async () => {
     throw error; // 오류를 다시 던져 호출자에게 알림
   }
 };
+
+// 아이템
+export const getItems = async () => {
+  try{
+    const latestVersion = await getLatestVersionUrl();
+    const res = await fetch(`${RIOT_BASE_URL}/cdn/${latestVersion}/data/ko_KR/item.json`);
+    const data = await res.json();
+    const itemList: ItemType[] = Object.values(data.data);
+    return itemList;
+  }catch(error){
+    console.error("아이템 API 에러:", error);    
+  }
+}
