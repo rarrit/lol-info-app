501eac4887e3fbcf1a7bb5c50f4ed2b5eb918674
김민규 <tjfvndfud@gmail.com>
2024-10-02 18:35:32 +0900

    add: getChampions 추가

--- src/app/utils/serverApi.ts
+++ src/app/utils/serverApi.ts
@@ -1,4 +1,5 @@
 import { RIOT_BASE_URL } from "../api/apiKey"
+import { ChampionInfo } from "../types/Champion";
 
 // 최신 버전
 export const getLatestVersionUrl = async () => {
@@ -8,3 +9,22 @@ export const getLatestVersionUrl = async () => {
   const latestVersion = version[0];
   return latestVersion;
 }
+
+
+// 챔피언
+export const getChampions = async () => {
+  try {
+    const latestVersion = await getLatestVersionUrl();
+    const res = await fetch(`${RIOT_BASE_URL}/cdn/${latestVersion}/data/ko_KR/champion.json`, {
+      next: {
+        revalidate: 86400,
+      }
+    });
+    const data = await res.json();
+    const championList: ChampionInfo[] = Object.values(data.data); 
+    return championList;
+  } catch (error) {
+    console.error('챔피언 데이터 가져오기 오류:', error);
+    throw error; // 오류를 다시 던져 호출자에게 알림
+  }
+};
