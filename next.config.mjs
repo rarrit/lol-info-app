47be6fde62ea6aafe2aac8e17f367ccfaa0ce5cb
김민규 <tjfvndfud@gmail.com>
2024-10-02 18:34:38 +0900

    add: remotePatterns 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ddragon.leagueoflegends.com", // url
+        pathname: "/**", // 전체
+      },
+    ],
+    formats: ["image/avif", "image/webp"], // 자동으로 포멧 변환
+  },
+};
 
 export default nextConfig;
