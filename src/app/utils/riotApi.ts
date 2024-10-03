1b21c9dbbc8d57f7c5bf6f9bab941aa733f5742b
김민규 <tjfvndfud@gmail.com>
2024-10-03 18:46:55 +0900

    add: server action 추가

---
+++ src/app/utils/riotApi.ts
@@ -0,0 +1,16 @@
+import { getChampions } from './serverApi';
+import { ChampionInfo } from '../types/Champion';
+
+export const getChampionRotation = async () => {
+  const champions = await getChampions();
+
+  const response = await fetch('/api/rotation');
+  const result = await response.json();
+  const freeChampionIds = result.data.freeChampionIds;
+
+  const rotation: ChampionInfo[] = Object.values(champions).filter((champion) =>
+    freeChampionIds.includes(Number(champion.key))
+  );
+
+  return rotation;
+};
