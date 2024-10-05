4a24332b685b62e70c16a4df99789c975be5c303
김민규 <tjfvndfud@gmail.com>
2024-10-06 05:03:33 +0900

    add: item 리스트, 상세 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         lol01: "url('/assets/images/bg-lol-01.jpg')",
         lol02: "url('/assets/images/bg-lol-02.jpg')",
         lol03: "url('/assets/images/bg-lol-03.jpg')",
+        lol04: "url('/assets/images/bg-lol-04.jpg')",
         icon01: "url('/assets/images/icon-logo.png')",
       },
       boxShadow: {
