7ceee41074f0e6d08329e59505725c11d8ff0abb
김민규 <tjfvndfud@gmail.com>
2024-10-07 14:22:47 +0900

    update: 배포테스트

--- src/app/utils/riotApi.ts
+++ src/app/utils/riotApi.ts
@@ -5,6 +5,7 @@ export const getChampionRotation = async () => {
   const champions = await getChampions();
   const response = await fetch('/api/rotation');
   const result = await response.json();
+  
   const freeChampionIds = result.data.freeChampionIds;
   const rotation: ChampionInfo[] = Object.values(champions).filter((champion) =>
     freeChampionIds.includes(Number(champion.key))
